0f7a4dd1771e1093c3cca363c9a4f005ecc26433
OTL <anik.saha@sslwireless.com>
2023-12-20 16:16:44 +0600

    dashboard data

--- services/dashboard.service.js
+++ services/dashboard.service.js
@@ -8,11 +8,13 @@ const { QueryTypes } = require('sequelize');
 const fetchDashboardInformation = async () => {
     const numberOfUpcomeingTour = await db.sequelize.query("SELECT COUNT(id) as totalUpcomeingTour FROM `tours` WHERE status='upcomeing'", { type: QueryTypes.SELECT });
     const numberOfCompletedTour = await db.sequelize.query("SELECT COUNT(id) as totalCompleteTour FROM `tours` WHERE status='complete'", { type: QueryTypes.SELECT });
-    const numberOfCardTypeTransection = await db.sequelize.query("SELECT cardtype,COUNT(id) FROM `transections` GROUP BY(cardtype);", { type: QueryTypes.SELECT });
+    const numberOfCardTypeTransection = await db.sequelize.query("SELECT cardtype,COUNT(id) as count FROM `transections` GROUP BY(cardtype);", { type: QueryTypes.SELECT });
+    const lastTransection = await db.sequelize.query("SELECT cardNo,cardholdername,name,amount FROM `transections` JOIN `users` WHERE transections.userId = users.id ORDER BY transections.id DESC;", { type: QueryTypes.SELECT });
     const result = {
-        numberOfUpcomeingTour:numberOfUpcomeingTour[0],
-        numberOfCompletedTour: numberOfCompletedTour[0],
-        numberOfCardTypeTransection : numberOfCardTypeTransection[0]
+        numberOfUpcomeingTour:numberOfUpcomeingTour[0].totalUpcomeingTour,
+        numberOfCompletedTour: numberOfCompletedTour[0].totalCompleteTour,
+        numberOfCardTypeTransection : numberOfCardTypeTransection[0].total,
+        lastTransection
     }
     return result;
 };
